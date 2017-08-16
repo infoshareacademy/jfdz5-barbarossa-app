7a68e0d7d91759509debad3f1ae1deba006ec5a3
Kamil Richert <kamil.richert92@gmail.com>
2017-08-16 21:05:27 +0200

    KR - create all components files and basis for app

---
+++ src/components/map-items/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Search.css';
+
+const Search = () => (
+    <div>
+        <form>
+            <p>Where:</p><input/>
+            <p>From:</p><input/>
+        </form>
+        <button>Search</button>
+    </div>
+);
+
+export default Search
