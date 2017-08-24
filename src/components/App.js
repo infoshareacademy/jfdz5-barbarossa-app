1b54961a72bad2c9d5df30bc84cc810e73a9ec04
AniaOstrowska <anna.kalinowska07@gmail.com>
2017-08-24 21:16:24 +0200

    prepare map component with google api

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Search from './map-items/Search.js'
 import Results from './map-items/Results.js'
 import Menu from './map-items/Menu.js'
+import Map from './map-items/Map.js'
 
 const App = () => (
     <div className="container">
@@ -13,6 +14,7 @@ const App = () => (
         </div>
         <div>
             <Menu/>
+            <Map/>
         </div>
     </div>
 
