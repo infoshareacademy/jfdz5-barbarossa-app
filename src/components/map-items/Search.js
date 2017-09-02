a8a452a01afd9fbbcc76a9915c33de438137408d
Kamil Richert <kamil.richert92@gmail.com>
2017-09-02 08:33:33 +0200

    feature/FZ5BR-56-KR add time-input to Search content

--- src/components/map-items/Search.js
+++ src/components/map-items/Search.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from 'react';
 import './Search.css';
+import 'font-awesome/css/font-awesome.min.css';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
-import {
-    Button
-} from 'react-bootstrap';
-import 'font-awesome/css/font-awesome.min.css';
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
+import moment from 'moment';
+import {Button} from 'react-bootstrap';
 
+const format = 'HH:mm';
+
+function onChange(value) {
+    value.format(format);
+}
 
 const options = [
     {value: 'one', label: 'One', matchPos: "start"},
@@ -28,37 +34,46 @@ class Search extends React.Component {
 
     render() {
         return (
-                <form className="search-container">
-                    <div className="search-box">
-                        <div className="search-box_icon">
-                            <i className="fa fa-map-marker"/>
-                        </div>
-                        <Select
-                            name="form-field-name"
-                            value={this.state.value}
-                            options={options}
-                            onChange={this.logChange}
-                            placeholder="Start point..."
-                            className="search-input"
-                        />
+            <form className="search-container">
+                <div className="search-box">
+                    <div className="search-box_icon">
+                        <i className="fa fa-map-marker"/>
                     </div>
-                    <div className="search-box">
-                        <div className="search-box_icon">
-                            <i className="fa fa-flag"/>
-                        </div>
+                    <Select
+                        name="form-field-name"
+                        value={this.state.value}
+                        options={options}
+                        onChange={this.logChange}
+                        placeholder="Start point..."
+                        className="search-input"
+                    />
+                </div>
+                <div className="search-box">
+                    <div className="search-box_icon">
+                        <i className="fa fa-flag"/>
+                    </div>
 
-                        <Select
-                            name="form-field-name"
-                            value={this.state.value}
-                            options={options}
-                            onChange={this.logChange}
-                            placeholder="Destination..."
-                            className="search-input"
-                        />
-                    </div>
+                    <Select
+                        name="form-field-name"
+                        value={this.state.value}
+                        options={options}
+                        onChange={this.logChange}
+                        placeholder="Destination..."
+                        className="search-input"
+                    />
+                </div>
+                <div className="search-box search-box__inverse">
+                    <TimePicker
+                        showSecond={false}
+                        defaultValue={moment()}
+                        onChange={onChange}
+                        format={format}
+                        className="search-time-input"
+                    />
                     <Button bsStyle="primary" className="search-button">
                         <i className="fa fa-search"/>Search</Button>
-                </form>
+                </div>
+            </form>
         )
     }
 
