6fbecd187424f069755e68ad0825d75e56f8a012
Kamil Richert <kamil.richert92@gmail.com>
2017-09-03 12:40:53 +0200

    feature/FZ5BR-56-KR add redux to app

---
+++ src/state/search.js
@@ -0,0 +1,17 @@
+// Action types
+
+// Action creators
+
+// Initial state
+const initialState = {
+    departureStop: null,
+    arrivalStop: null,
+}
+
+// Reducer
+export default (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
