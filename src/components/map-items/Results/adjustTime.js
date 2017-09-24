1ba4222d98a23066d305dd90d5acfafdb4fb0855
Kamil Richert <kamil.richert92@gmail.com>
2017-09-24 20:01:50 +0200

    refactoring

--- src/components/map-items/Results/adjustTime.js
+++ src/components/map-items/Results/adjustTime.js
@@ -1,12 +1,21 @@
 export const adjustTime = (departures, type, inputTime) => {
     return departures.every(
-        departure => type === 'arrival' ?
-            departure.depFromEndStop : departure.depFromStartStop <= inputTime
-    ) === false ?
+        ({ depFromEndStop, depFromStartStop }) => (
+            inputTime >= (
+                type === 'arrival' ?
+                    depFromEndStop :
+                    depFromStartStop
+            )
+        )
+    ) ?
+        departures.reduce((min, next) => Math.min(min, next), 0) :
         departures.filter(
-            departure => type === 'arrival' ?
-                departure.depFromEndStop : departure.depFromStartStop >= inputTime
-        ).reduce((min, next) => min <= next ? min : next, 0)
-        :
-        departures.reduce((min, next) => min <= next ? min : next, 0)
+            ({ depFromEndStop, depFromStartStop }) => (
+                (
+                    type === 'arrival' ?
+                        depFromEndStop :
+                        depFromStartStop
+                ) >=  inputTime
+            )
+        ).reduce((min, next) => Math.min(min, next), 0)
 }
