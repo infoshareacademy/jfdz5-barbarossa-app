8d0e593d64fbe36148ee0c8b2a7463613afd8412
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-09-27 16:20:17 +0200

    logged section styles

--- src/components/menu-items/Log.js
+++ src/components/menu-items/Log.js
@@ -5,17 +5,22 @@ import AuthLog from '../AuthLog'
 import SignInForm from './SignInForm'
 import SignUpForm from './SignUpForm'
 
+import './Log.css'
+
+
 const Log = () => (
-    <AuthLog>
-        <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
-            <Tab eventKey={1} title="Sign In">
-                <SignInForm/>
-            </Tab>
-            <Tab eventKey={2} title="Sign Up">
-                <SignUpForm/>
-            </Tab>
-        </Tabs>
-    </AuthLog>
+    <div className="log">
+        <AuthLog>
+            <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+                <Tab eventKey={1} title="Sign In">
+                    <SignInForm/>
+                </Tab>
+                <Tab eventKey={2} title="Sign Up">
+                    <SignUpForm/>
+                </Tab>
+            </Tabs>
+        </AuthLog>
+    </div>
 );
 
 export default Log
